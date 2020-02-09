025f7abf1398a742efc8bb61108581b85b57545f
Denys Zabudskyi <deniszabudsky@gmail.com>
2020-02-09 23:23:16 +0200

    CN-001

---
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
+import MyPosts from "./MyPosts";
+import {connect} from "react-redux";
+
+let mapStateToProps = (state) => {
+    return {
+        newPostText: state.profilePage.newPostText,
+        posts: state.profilePage.posts
+    }
+};
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewPostText: (text) => {
+            dispatch( updateNewPostTextActionCreator(text) );
+        },
+        addPost : () => {
+            dispatch(addPostActionCreator());
+        },
+    }
+};
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
+
+export default MyPostsContainer;
